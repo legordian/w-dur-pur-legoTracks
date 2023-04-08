556bf82c87bad1ec43cac80eddd37b8512983811
Karl Bicker <legordian@gmx.net>
2023-04-08 15:44:08 +0200

    Numpy-ify and cleanup.

--- a_star.py
+++ a_star.py
@@ -2,16 +2,47 @@
 import collections
 import math
 import matplotlib.pyplot
+import numpy
+import numpy.linalg
 import enum
 
 
-coordClass = collections.namedtuple("coordClass", ["x", "y", "angle"])
-nodeClass = collections.namedtuple("nodeClass", ["coords", "parentNode", "g", "h"])
+#coordClass = collections.namedtuple("coordClass", ["x", "y", "angle"])
+class coordClass:
+
+
+    def __init__(self, vec, angle):
+        self.vec = vec
+        self.angle = angle
+
+    @classmethod
+    def fromXYCoords(self, x, y, angle):
+        vec = numpy.array([x, y])
+        return coordClass(vec, angle)
+
+    @property
+    def x(self):
+        return self.vec[0]
+
+    @x.setter
+    def x(self, val):
+        self.vec[0] = val
+
+    @property
+    def y(self):
+        return self.vec[1]
+
+    @y.setter
+    def y(self, val):
+        self.vec[1] = val
+
+
+nodeClass = collections.namedtuple("nodeClass", ["coords", "parentNode", "g", "h", "f"])
 
 Direction = enum.Enum('Direction', ['LEFT', 'STRAIGHT', 'RIGHT'])
 
-leftCurveVec = coordClass(2.5 * math.sin(math.pi / 8.), 2.5 * (1 - math.cos(math.pi / 8.)), None)
-rightCurveVec = coordClass(leftCurveVec.x, -leftCurveVec.y, None)
+leftCurveVec = coordClass.fromXYCoords(2.5 * numpy.sin(numpy.pi / 8.), 2.5 * (1 - numpy.cos(numpy.pi / 8.)), None)
+rightCurveVec = coordClass.fromXYCoords(leftCurveVec.x, -leftCurveVec.y, None)
 
 matplotlib.pyplot.ion()
 fig = matplotlib.pyplot.figure()
@@ -19,41 +50,22 @@ ax = fig.add_subplot(111)
 
 
 def dist(coords1, coords2):
-    return math.sqrt(math.pow((coords1.x - coords2.x), 2) + math.pow((coords1.y - coords2.y), 2))
+    return numpy.linalg.norm(coords2.vec - coords1.vec)
+    #return math.sqrt(math.pow((coords1.x - coords2.x), 2) + math.pow((coords1.y - coords2.y), 2))
 
 
-def h(currentCoords, goalCoords):
-
-    return dist(currentCoords, goalCoords)
-
-    retval = math.pow((goalCoords.x - currentCoords.x), 2) + math.pow((goalCoords.y - currentCoords.y), 2)
-    angleDiff = goalCoords.angle - currentCoords.angle
-    if angleDiff > math.pi: angleDiff = 2 * math.pi - angleDiff
-    retval += pow(angleDiff / (math.pi / 8.), 2) / pow(0.8 + currentCoords.x, 4) #der geht!!
-    # retval += pow((goalCoords.angle - currentCoords.angle) / (math.pi / 8.), 2) / pow(retval, 2)
-    # retval += pow(angleDiff / (math.pi / 8.), 2) / 5.
-    # retval += pow((currentCoords.angle + goalCoords.angle) / (math.pi / 8.), 2) / pow(retval, 2)
-    # # retval /= math.log(currentCoords.y + 1) if currentCoords.y > -1 else 1
-    # if currentCoords.y < -1: retval *= 100000
-    # if currentCoords.x < 0: retval *= 100000
-    # if currentCoords.x < 2 and currentCoords.y > 2: retval *= 100000
-    if currentCoords.y < -2.5 and currentCoords.x < 2.5: retval *= 100000
-    if math.pow(currentCoords.x, 2) + math.pow(currentCoords.y + 2.5, 2) < 6.25: retval += 100000
-    if currentCoords.y > 2.5 and currentCoords.x < 2.5: retval *= 100000
-    if math.pow(currentCoords.x, 2) + math.pow(currentCoords.y - 2.5, 2) < 6.25: retval += 100000
-    if currentCoords.y > 7 or currentCoords.y < -7: retval *= 100000
-
-    # if currentCoords.x < 4 and abs(currentCoords.angle) > math.pi * 0.6: retval *= 100000
-    # # if currentCoords.angle > math.pi * 0.5: retval *= 100000
-    return retval
+def calc_h(currentCoords, goalCoords):
+    # return dist(currentCoords, goalCoords)
+    diff = goalCoords.vec - currentCoords.vec
+    return numpy.inner(diff, diff)
 
 
-def f(node):
-    return node.g + node.h
+# def f(node):
+#     return node.g + node.h
 
 
 def coordsEqual(coords1, coords2):
-    return coords1.x == coords2.x and coords1.y == coords2.y and coords1.angle == coords2.angle
+    return numpy.array_equal(coords1.vec, coords2.vec)
 
 
 def nodesEqual(node1, node2):
@@ -61,31 +73,44 @@ def nodesEqual(node1, node2):
 
 
 def rotateCoords(coords, angle): # angle > 0 -> counterclockwise
-    return coordClass(coords.x * math.cos(angle) - coords.y * math.sin(angle), coords.x * math.sin(angle) + coords.y * math.cos(angle), None)
+    rot = numpy.array([[numpy.cos(angle), -numpy.sin(angle)], [numpy.sin(angle), numpy.cos(angle)]])
+    return numpy.dot(rot, coords.vec)
+    # newVec = numpy.dot(rot, coords.vec)
+    # return coordClass(newVec, None)
+    # return coordClass(coords.x * math.cos(angle) - coords.y * math.sin(angle), coords.x * math.sin(angle) + coords.y * math.cos(angle), None)
 
 
 def cleanAngle(angle):
-    deg = round(math.degrees(angle), 3)
-    if abs(deg) > 180:
-        deg = deg % (180 if deg < 0 else -180)
-    if deg == -180: deg = 180
-    return math.radians(deg)
+    deg = numpy.round(angle / numpy.pi * 180, 3)
+    if numpy.abs(deg) > 180:
+        deg = numpy.mod(deg, 180 if deg < 0 else -180)
+    if deg == -180.: deg = numpy.double(180)
+    return deg / 180. * numpy.pi
+    # deg = round(math.degrees(angle), 3)
+    # if abs(deg) > 180:
+    #     deg = deg % (180 if deg < 0 else -180)
+    # if deg == -180: deg = 180
+    # return math.radians(deg)
 
 
 def propagateCoords(d, coords):
+    addVec = None
+    newAngle = None
     if d == Direction.LEFT:
         addVec = rotateCoords(leftCurveVec, coords.angle)
-        return coordClass(coords.x + addVec.x, coords.y + addVec.y, cleanAngle(coords.angle + math.pi/8.))
+        newAngle = cleanAngle(coords.angle + numpy.pi/8.)
+        # return coordClass(coords.vec + addVec, cleanAngle(coords.angle + numpy.pi/8.))
     elif d == Direction.STRAIGHT:
-        return coordClass(coords.x + math.cos(coords.angle), coords.y + math.sin(coords.angle), coords.angle)
+        addVec = numpy.array([numpy.cos(coords.angle), numpy.sin(coords.angle)])
+        newAngle = coords.angle
+        # return coordClass(coords.x + numpy.cos(coords.angle), coords.y + numpy.sin(coords.angle), coords.angle)
     elif d == Direction.RIGHT:
         addVec = rotateCoords(rightCurveVec, coords.angle)
-        # print(f"({rightCurveVec.x}, {rightCurveVec.y})")
-        # print(math.degrees(coords.angle))
-        # print(f"({addVec.x}, {addVec.y})")
-        return coordClass(coords.x + addVec.x, coords.y + addVec.y, cleanAngle(coords.angle - math.pi/8.))
+        newAngle = cleanAngle(coords.angle - numpy.pi/8.)
+        # return coordClass(coords.vec + addVec, cleanAngle(coords.angle - numpy.pi/8.))
     else:
         raise Exception("ups")
+    return coordClass(coords.vec + addVec, newAngle)
 
 
 def nodeToStr(node):
@@ -101,11 +126,6 @@ def buildPath(node, path = []):
 def updatePlot(path):
     if not path:
         return
-
-    # print("path:")
-    # for p in path:
-    #     print("\t" + nodeToStr(p))
-
     ax.plot([p.coords.x for p in path], [p.coords.y for p in path])
     ax.set_xlim([0, 10])
     ax.set_ylim([-7, 7])
@@ -122,66 +142,40 @@ def addNodePathPlot(node1, node2):
 
 
 def run_a_star(start, end):
-    startNode = nodeClass(start, None, 0, h(start, end))
+    startNode = nodeClass(start, None, 0, calc_h(start, end), calc_h(start, end))
     openList = [ startNode ]
     closedList = []
     i = 0
     while openList:
-        # print("iteration " + str(i))
-        # if i > 20: break
-
-        # print("open list:")
-        # for p in openList:
-        #     print("\t" + nodeToStr(p))
-
-        currentNode = min(openList, key=f)
-
-        # print("currentNode: " + nodeToStr(currentNode) + "\n")
-
+        currentNode = min(openList, key=lambda x: x.f)
         if i % 1 == 0:
             updatePlot(buildPath(currentNode))
         if i % 50 == 0:
             ax.clear()
-        # print("at node " + nodeToStr(currentNode))
         openList.remove(currentNode)
         closedList.append(currentNode)
-        # if currentNode.parentNode is not None:
-        #     addNodePathPlot(currentNode.parentNode, currentNode)
         if dist(currentNode.coords, end) < 0.3 and abs(currentNode.coords.angle - math.pi) < 0.01:
-        # if coordsEqual(currentNode.coords, end):
             print("found it")
-            break
             print(nodeToStr(currentNode))
             for n in buildPath(currentNode):
                 print("\t" + nodeToStr(n))
         for d in list(Direction):
-            # if d != Direction.RIGHT:
-            #     continue
-            # if currentNode.g > 15 and d == Direction.LEFT:
-            #     continue
-            # if currentNode.g == 0 and d != Direction.LEFT:
-            #     continue
             newCoords = propagateCoords(d, currentNode.coords)
-            node = nodeClass(newCoords, currentNode, currentNode.g + 1, h(newCoords, end))
+            g = currentNode.g + 1
+            h = calc_h(newCoords, end)
+            node = nodeClass(newCoords, currentNode, g, h, g + h)
             if [ x for x in closedList if coordsEqual(x.coords, newCoords)]:
                 continue
             filteredOpenList = [ x for x in openList if coordsEqual(x.coords, newCoords) and node.g > x.g ]
             if filteredOpenList:
                 continue
-            # print("\tappending node " + nodeToStr(node))
-            # if f(node) < f(currentNode):
-            #     addNodePathPlot(currentNode, node)
             openList.append(node)
         i += 1
 
 
 def main():
-    start = coordClass(0., 0.5, 0.)
-    end = coordClass(0., 0., math.pi)
-    # start = coordClass(0., 0., 0.)
-    # end = coordClass(0., -5., math.pi)
-    # end = coordClass(0., 0., math.pi)
-    # end = coordClass(4., 0., math.pi)
+    start = coordClass.fromXYCoords(0., 0.5, numpy.double(0.))
+    end = coordClass.fromXYCoords(0., 0., numpy.pi)
     run_a_star(start, end)
     input()
 
